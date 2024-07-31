f290697637b88e595144405d50f3066d2f685e69
LMacrini <62901118+LMacrini@users.noreply.github.com>
2024-07-31 17:22:05 -0400

    remove console logs

--- src/SimpleCollection/components/Header.jsx
+++ src/SimpleCollection/components/Header.jsx
@@ -66,7 +66,7 @@ export default (props) => {
 
             {dividedContent[0].map(element => {
             const text = element.content[0].text.trim().split('\n');
-            // console.log("text and text length: ", text, text.length)
+
             if (text.length === 1) {
               return <path d={text[0]} />; 
             } else {
