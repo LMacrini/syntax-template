d20723c482df75ba9b947f1cce0948ae06f95658
LMacrini <62901118+LMacrini@users.noreply.github.com>
2024-08-01 11:10:51 -0400

    add callout component

--- src/SimpleCollection/index.js
+++ src/SimpleCollection/index.js
@@ -1,7 +1,8 @@
 // import local components
 import Block from './components/Block';
 import Header from './components/Header';
+import Callout from './components/Callout';
 // import CSS
 import './index.css';
 
-export default { Block, Header };
+export default { Block, Header, Callout };
