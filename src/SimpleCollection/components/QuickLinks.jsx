2c01bcdd5f60c2077ce57a9cf41348175d1d3eb5
LMacrini <62901118+LMacrini@users.noreply.github.com>
2024-08-01 16:51:36 -0400

    QuickLinks and adjust schemas

---
+++ src/SimpleCollection/components/QuickLinks.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function QuickLinks(props) {
+  console.log(props);
+  return (
+    <div className="not-prose my-12 grid grid-cols-1 gap-6 sm:grid-cols-2">
+    
+    </div>
+  )
+}
--- src/SimpleCollection/index.js
+++ src/SimpleCollection/index.js
@@ -3,7 +3,9 @@ import Block from './components/Block';
 import Header from './components/Header';
 import Callout from './components/Callout';
 import Button from './components/Button';
+import QuickLinks from './components/QuickLinks';
+import QuickLink from './components/QuickLink';
 // import CSS
 import './index.css';
 
-export default { Block, Header, Callout, Button };
+export default { Block, Header, Callout, Button, QuickLinks, QuickLink };
