46186609754d4d2c6397f129ed418cb67d4dae35
LMacrini <62901118+LMacrini@users.noreply.github.com>
2024-08-01 15:35:43 -0400

    Finalize button

--- src/SimpleCollection/components/Button.jsx
+++ src/SimpleCollection/components/Button.jsx
@@ -8,8 +8,8 @@ const variantStyles = {
     'rounded-full bg-slate-800 py-2 px-4 text-sm font-medium text-white hover:bg-slate-700 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white/50 active:text-slate-400',
 }
 
-function EmptyButton( props ) {
-  const variant = 'primary'
+function EmptyButton( { variant, ...props } ) {
+  // const variant = 'primary'
   let className = ''
   className = clsx(variantStyles[variant], className)
 
@@ -21,9 +21,7 @@ function EmptyButton( props ) {
 }
 
 export default function Button( props ) {
-    const { block: { main: { body: { paragraphs, links } } } } = props;
-    return <EmptyButton href={links[0]?.href} {...props}>{[links.map(link => link.label).join('\n').trim(), paragraphs.join('\n').trim()].join('\n')}</EmptyButton>
+    const { type, href } = props.block.getBlockProperties();
+    const { block: { main: { body: { paragraphs } } } } = props;
+    return <EmptyButton href={href} variant={type} {...props}>{paragraphs.join('\n').trim()}</EmptyButton>
 }
-
-// Okay so i know this code looks impossible to read but trust me it's simple. Basically, if the docufolio has multiple links and paragraphs, the button will lead to the first link.
-// The button itself will also containt all of the text for each, but the links will be first. See? simple.
