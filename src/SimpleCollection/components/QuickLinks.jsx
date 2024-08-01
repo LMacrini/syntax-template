a15a5ab72d0a1fd1957f7caccac5ea89956ca32f
LMacrini <62901118+LMacrini@users.noreply.github.com>
2024-08-01 17:22:19 -0400

    get quicklinks to render children

--- src/SimpleCollection/components/QuickLinks.jsx
+++ src/SimpleCollection/components/QuickLinks.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 export default function QuickLinks(props) {
-  console.log(props);
+  const { block } = props;
+  const ChildBlocks = block.getChildBlockRenderer();
   return (
     <div className="not-prose my-12 grid grid-cols-1 gap-6 sm:grid-cols-2">
-    
+      <ChildBlocks block={block} />
     </div>
   )
 }
