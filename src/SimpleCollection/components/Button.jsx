db7f87a34d00e2c8129aa89dd5380acbf7da4e97
LMacrini <62901118+LMacrini@users.noreply.github.com>
2024-08-01 12:32:31 -0400

    properly add button

--- src/SimpleCollection/components/Button.jsx
+++ src/SimpleCollection/components/Button.jsx
@@ -8,7 +8,7 @@ const variantStyles = {
     'rounded-full bg-slate-800 py-2 px-4 text-sm font-medium text-white hover:bg-slate-700 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white/50 active:text-slate-400',
 }
 
-export function Button( props ) {
+export default function Button( props ) {
   className = clsx(variantStyles[variant], className)
 
   return typeof props.href === 'undefined' ? (
--- src/SimpleCollection/index.js
+++ src/SimpleCollection/index.js
@@ -2,7 +2,8 @@
 import Block from './components/Block';
 import Header from './components/Header';
 import Callout from './components/Callout';
+import Button from './components/Button';
 // import CSS
 import './index.css';
 
-export default { Block, Header, Callout };
+export default { Block, Header, Callout, Button };
