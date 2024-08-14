a54e007fdf49a09791d84e4ce0fe8ef7526c0f1a
LMacrini <62901118+LMacrini@users.noreply.github.com>
2024-08-14 16:35:01 -0400

    create hero component

--- src/SimpleCollection/index.js
+++ src/SimpleCollection/index.js
@@ -1,19 +1,32 @@
 // import local components
-import Block from './components/Block';
-import Header from './components/Header';
-import Callout from './components/Callout';
-import Button from './components/Button';
-import QuickLinks from './components/QuickLinks';
-import QuickLink from './components/QuickLink';
-import Layout from './components/Layout';
-import LeftPanel from './components/LeftPanel';
-import RightPanel from './components/RightPanel';
-import Section from './components/Section';
-import Footer from './components/Footer';
+import Block from "./components/Block";
+import Header from "./components/Header";
+import Callout from "./components/Callout";
+import Button from "./components/Button";
+import QuickLinks from "./components/QuickLinks";
+import QuickLink from "./components/QuickLink";
+import Layout from "./components/Layout";
+import LeftPanel from "./components/LeftPanel";
+import RightPanel from "./components/RightPanel";
+import Section from "./components/Section";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 
 // import CSS
-import './index.css';
+import "./index.css";
 
-export default { Block, Header, Callout, Button, QuickLinks, QuickLink, LeftPanel, RightPanel, Section, Footer };
+export default {
+	Block,
+	Header,
+	Callout,
+	Button,
+	QuickLinks,
+	QuickLink,
+	LeftPanel,
+	RightPanel,
+	Section,
+	Footer,
+	Hero,
+};
 
 export const site = { Layout };
