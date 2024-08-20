12ef95c6b0294369336a8e1004919a0108bed578
LMacrini <62901118+LMacrini@users.noreply.github.com>
2024-08-20 10:16:55 -0400

    Update tailwind.config.js

--- src/SimpleCollection/tailwind.config.js
+++ src/SimpleCollection/tailwind.config.js
@@ -40,6 +40,21 @@ module.exports = {
     content: ['../src/**/*.{js,jsx}', makeEntryPath('@uniwebcms/module-sdk'), makeEntryPath('flowbite-react')],
     plugins: [require('@tailwindcss/line-clamp'), require("@tailwindcss/typography"), makeEntryPath('@uniwebcms/module-sdk', 'plugin')],
     theme: {
+        fontSize: {
+            xs: ['0.75rem', { lineHeight: '1rem' }],
+            sm: ['0.875rem', { lineHeight: '1.5rem' }],
+            base: ['1rem', { lineHeight: '2rem' }],
+            lg: ['1.125rem', { lineHeight: '1.75rem' }],
+            xl: ['1.25rem', { lineHeight: '2rem' }],
+            '2xl': ['1.5rem', { lineHeight: '2.5rem' }],
+            '3xl': ['2rem', { lineHeight: '2.5rem' }],
+            '4xl': ['2.5rem', { lineHeight: '3rem' }],
+            '5xl': ['3rem', { lineHeight: '3.5rem' }],
+            '6xl': ['3.75rem', { lineHeight: '1' }],
+            '7xl': ['4.5rem', { lineHeight: '1' }],
+            '8xl': ['6rem', { lineHeight: '1' }],
+            '9xl': ['8rem', { lineHeight: '1' }],
+          },
         extend: {
             colors: {
                 ...generateThemeConfig({
@@ -59,10 +74,13 @@ module.exports = {
                     'btn-hover-text-color'
                 ])
             },
-            spacing: {
-                '8xl': '96rem',
-                '9xl': '108rem'
-            }
+            maxWidth: {
+                '8xl': '88rem',
+              },
+            // spacing: {
+            //     '8xl': '96rem',
+            //     '9xl': '108rem'
+            // }
         }
     }
 };
