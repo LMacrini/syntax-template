199efdc945a7d2aad894d8eeb0b8366a63b09398
LMacrini <62901118+LMacrini@users.noreply.github.com>
2024-08-21 14:35:06 -0400

    remove useless ids

--- src/SimpleCollection/components/Layout/index.jsx
+++ src/SimpleCollection/components/Layout/index.jsx
@@ -7,12 +7,9 @@ export default function Layout(props) {
 	const { page, header, footer, body, leftPanel, rightPanel } = props;
 
 	return (
-		<div id="main" className="flex w-full flex-col">
+		<div className="flex w-full flex-col">
 			{header}
-			<div
-				id="content"
-				className="relative mx-auto flex w-full max-w-8xl flex-auto justify-center sm:px-2 lg:px-8 xl:px-12"
-			>
+			<div className="relative mx-auto flex w-full max-w-8xl flex-auto justify-center sm:px-2 lg:px-8 xl:px-12">
 				{leftPanel}
 				<div className="min-w-0 max-w-2xl flex-auto px-4 py-16 lg:max-w-none lg:pl-8 lg:pr-0 xl:px-16">
 					<article>
